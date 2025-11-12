09688b1a4d64e593dcb0b5579581f594d84808b7
Mikhail Baskakov <m.baskakov@f6.ru>
2025-11-12 05:13:47 -0500

    fix DependencyManagement

--- DependencyManagement.cmake
+++ DependencyManagement.cmake
@@ -2,10 +2,10 @@ cmake_minimum_required(VERSION 3.26.0)
 
 include(FetchContent)
 
-if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Cmake/FindOrCloneDependency.cmake")
-    include(FindOrCloneDependency)
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/FindOrCloneDependency.cmake")
+    include(FindOrCloneDependency REQUIRED)
     set(CUSTOM_DEP_HANDLER_INCLUDED TRUE)
 else()
-    message(WARNING "DependencyManagement: cmake/FindOrCloneDependency.cmake not found.")
+    message(WARNING "DependencyManagement: FindOrCloneDependency.cmake not found.")
     set(CUSTOM_DEP_HANDLER_INCLUDED FALSE)
 endif()
